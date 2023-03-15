5c8dd76122ab5d44623e712ccdbd57d71cb7238c
VictorySamoletova <victoryer@mail.ru>
2023-03-16 01:19:31 +0300

    New second Androidproj layoutParams

---
+++ app/src/main/java/ru/sinergy/androidstartproj/MainActivity.java
@@ -0,0 +1,32 @@
+package ru.sinergy.androidstartproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+        TextView textView = new TextView(this);
+        textView.setText("Hello, Android!");
+        textView.setTextSize(26);
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+
+        textView.setLayoutParams(layoutParams);
+
+        constraintLayout.addView(constraintLayout);
+    }
+}
