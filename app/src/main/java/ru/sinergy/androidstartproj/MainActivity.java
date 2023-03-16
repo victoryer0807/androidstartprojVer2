ff09215e1b7762c32528c7f117988ddc15bc8463
VictorySamoletova <victoryer@mail.ru>
2023-03-16 06:58:36 +0300

    Added third Java screen configuration example with Constraint Layout

--- app/src/main/java/ru/sinergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/sinergy/androidstartproj/MainActivity.java
@@ -11,24 +11,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
 
-        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-        TextView textView = new TextView(this);
-        textView.setText("Hello, Android!");
-        textView.setTextSize(26);
-        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-                ConstraintLayout.LayoutParams.WRAP_CONTENT);
 
-        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+        TextView textView = (TextView) findViewById(R.id.header);
+        textView.setText("Hello from Java");
 
-        textView.setLayoutParams(layoutParams);
-
-        constraintLayout.addView(textView);
-        setContentView(constraintLayout);
+//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+//        TextView textView = new TextView(this);
+//        textView.setText("Hello, Android!");
+//        textView.setTextSize(26);
+//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+//
+//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+//
+//        textView.setLayoutParams(layoutParams);
+//
+//        constraintLayout.addView(textView);
+//        setContentView(constraintLayout);
 
     }
 }
