07cefc7ffb1809a2e7d4b2b8567f386f406d8937
VictorySamoletova <victoryer@mail.ru>
2023-03-16 16:09:28 +0300

    Clean Up of project

--- app/src/main/java/ru/sinergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/sinergy/androidstartproj/MainActivity.java
@@ -14,25 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.second_layout);
 
 
-//        TextView textView = (TextView) findViewById(R.id.header);
-//        textView.setText("Hello from Java");
-
-//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-//        TextView textView = new TextView(this);
-//        textView.setText("Hello, Android!");
-//        textView.setTextSize(26);
-//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
 //
-//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
-//
-//        textView.setLayoutParams(layoutParams);
-//
-//        constraintLayout.addView(textView);
-//        setContentView(constraintLayout);
-
     }
 }
