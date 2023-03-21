a75fe664216ec1cd033ca040a99c1df17b78f688
VictorySamoletova <victoryer@mail.ru>
2023-03-21 23:04:15 +0300

    our first calculator done

--- app/src/main/java/ru/sinergy/androidstartproj/Calculator.java
+++ app/src/main/java/ru/sinergy/androidstartproj/Calculator.java
@@ -3,6 +3,12 @@ package ru.sinergy.androidstartproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Calculator extends AppCompatActivity {
 
@@ -11,51 +17,53 @@ public class Calculator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        final Button calculate = (Button) findViewById(R.id.calc);
-
-        calculate.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                calculateAnswer();
-            }
-        });
-    }
-    private void calculateAnswer(){
-        EditText numOne = (EditText) findViewById(R.id.editTextNumberDecimal);
-        EditText numTwo = (EditText) findViewById(R.id.editTextNumberDecimal2);
-
-        RadioButton add = (RadioButton) findViewById(R.id.add);
-        RadioButton sub = (RadioButton) findViewById(R.id.subtract);
-        RadioButton multiply = (RadioButton) findViewById(R.id.multiple);
-        RadioButton divide = (RadioButton) findViewById(R.id.divide);
-
-        TextView answer = (TextView) findViewById(R.id.result);
-
-        float numone = Integer.parseInt(numOne.getText().toString());
-        float numtwo = Integer.parseInt(numTwo.getText().toString());
-
-        float solution = 0;
-
-        if(add.isChecked()){
-            solution = numone + numtwo;
+    final Button calculate = (Button) findViewById(R.id.calc);
+    calculate.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            calculateAnswer();
         }
-        if(sub.isChecked()){
-            solution = numone - numtwo;
-        }
-        if(multiply.isChecked()){
-            solution = numone * numtwo;
-        }
-        if(divide.isChecked()){
-            if(numtwo == 0){
-                Toast.makeText(this, "Number two cannot be zero", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            solution = numone / numtwo;
-        }
-       answer.setText("The answer is "+ solution);
+    });
+
 
     }
+private void calculateAnswer(){
+    EditText numOne = (EditText) findViewById(R.id.editTextNumberDecimal);
+    EditText numTwo = (EditText) findViewById(R.id.editTextNumberDecimal2);
 
+    RadioButton add = (RadioButton) findViewById(R.id.add);
+    RadioButton sub = (RadioButton) findViewById(R.id.subtract);
+    RadioButton multiply = (RadioButton) findViewById(R.id.multiple);
+    RadioButton divide = (RadioButton) findViewById(R.id.divide);
+
+    TextView answer = (TextView) findViewById(R.id.result);
+
+
+    float numone = Integer.parseInt(numOne.getText().toString());
+    float numtwo = Integer.parseInt(numTwo.getText().toString());
+
+    float solution = 0;
+
+    if(add.isChecked()){
+        solution = numone + numtwo;
+    }
+
+    if(sub.isChecked()){
+        solution = numone - numtwo;
+    }
+    if(multiply.isChecked()){
+        solution = numone * numtwo;
+    }
+    if(divide.isChecked()){
+        if(numtwo == 0){
+            Toast.makeText(this, "Number two cannot be zero", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        solution = numone / numtwo;
+    }
+
+    answer.setText("The answer is "+solution);
+}
 
 
 }
