58512175579095e87b9d04a65f3be1a98aea757c
VictorySamoletova <victoryer@mail.ru>
2023-03-24 01:06:39 +0300

    Finished to Activity ComeBack

--- app/src/main/java/ru/sinergy/androidstartproj/ComeBackActivity.java
+++ app/src/main/java/ru/sinergy/androidstartproj/ComeBackActivity.java
@@ -1,14 +1,35 @@
 package ru.sinergy.androidstartproj;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ComeBackActivity extends AppCompatActivity {
+
+    EditText textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_come_back);
+
+
+       textView = (EditText) findViewById(R.id.backtv);
+       button = (Button) findViewById(R.id.sendback);
+
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent();
+               intent.putExtra("tv",textView.getText().toString());
+               setResult(RESULT_OK,intent);
+               finish();
+           }
+       });
     }
 }
--- app/src/main/java/ru/sinergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/sinergy/androidstartproj/MainActivity.java
@@ -8,11 +8,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final int REQ_C = 1;
     EditText et;
     private TextView tv;
+
+    ActivityResultLauncher<Intent> mStartActivityFotResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Intent intent = result.getData();
+                    tv.setText(intent.getStringExtra("tv"));
+                }
+            }
+    );
 
 
     @Override
@@ -47,9 +65,10 @@ public class MainActivity extends Activity implements View.OnClickListener{
                     i.putExtra("et", eText);
                     startActivity(i);
                     break;
-//                case R.id.button3:
-//                    i = new Intent(this, ComeBackActivity.class);
-//                    startActivityForResult(i, REQ_C);
+                case R.id.button3:
+                    i = new Intent(this, ComeBackActivity.class);
+                    startActivityForResult(i, REQ_C);
+                    mStartActivityFotResult.launch(i);
             }
 
         }
